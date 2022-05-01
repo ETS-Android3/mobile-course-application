9ce75eca6d6ecbe2af73834799d403e6b676bf9e
Furkan Akcakaya <akcakayafurkan@yandex.com>
2022-05-01 17:26:17 +0300

    player re-added

--- app/src/main/java/com/example/mobile30_03/musicfragments/PlayerFragment.java
+++ app/src/main/java/com/example/mobile30_03/musicfragments/PlayerFragment.java
@@ -10,16 +10,29 @@ import android.view.ViewGroup;
 
 import com.example.mobile30_03.R;
 import com.example.mobile30_03.databinding.FragmentPlayerBinding;
+import com.example.mobile30_03.models.Music;
+import com.example.mobile30_03.utils.HelperFunctions;
+import com.example.mobile30_03.utils.MediaPlayerManager;
 
 public class PlayerFragment extends Fragment {
+    public PlayerFragment() {}
+    private static final String TAG = "SongsFragment";
     private FragmentPlayerBinding binding;
 
-    public PlayerFragment() {}
+    //Create MediaPlayerManager
+    private MediaPlayerManager mediaPlayerManager = MediaPlayerManager.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentPlayerBinding.inflate(inflater, container, false);
+        Music currentSong = mediaPlayerManager.getMusicList()
+                .get(mediaPlayerManager.getCurrentSongIndex());
+
+        binding.tvArtistName.setText(currentSong.getArtist_name());
+        binding.tvDurationTotal.setText(HelperFunctions.milliSecondsToTimer(currentSong.getDuration()));
+        binding.tvSongName.setText(currentSong.getSong_name());
+
         return binding.getRoot();
     }
 }
